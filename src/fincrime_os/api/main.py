2b168f58a5cd6c7e2262c989204c1cf4eb8da499
Mfalme <calebrutto91@gmail.com>
2026-09-17 11:55:23 +0300

    Bump platform version to 0.2.3 in FastAPI app and update test assertion

--- src/fincrime_os/api/main.py
+++ src/fincrime_os/api/main.py
@@ -30,7 +30,7 @@ from fincrime_os.pipeline import Pipeline
 configure_logging()
 log = logging.getLogger("fincrime_os.api")
 
-app = FastAPI(title="FINCRIME OS", version="0.2.2")
+app = FastAPI(title="FINCRIME OS", version="0.2.3")
 
 from fincrime_os.state.graph_snapshot import load_snapshot  # noqa: E402
 
