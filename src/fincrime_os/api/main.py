363d39f6c824d01ae6e55599d877fa5efa652f90
Mfalme <calebrutto91@gmail.com>
2026-09-17 18:43:38 +0300

    Bump FastAPI version to 0.3.1 in main application

--- src/fincrime_os/api/main.py
+++ src/fincrime_os/api/main.py
@@ -31,7 +31,7 @@ from fincrime_os.pipeline import Pipeline
 configure_logging()
 log = logging.getLogger("fincrime_os.api")
 
-app = FastAPI(title="FINCRIME OS", version="0.2.3")
+app = FastAPI(title="FINCRIME OS", version="0.3.1")
 
 from fincrime_os.state.graph_snapshot import load_snapshot  # noqa: E402
 
